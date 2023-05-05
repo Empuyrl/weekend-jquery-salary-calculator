217082943099ce1098c40c5fcdd754e986cc307d
User <user@Users-MacBook-Pro.local>
2023-05-05 17:20:32 -0500

    sourced in all of the files. Added a readyNow and the first function. Added a bunch of inputs, a couple buttons and a table for data. Lots of infomation

---
+++ javascript.js
@@ -0,0 +1,10 @@
+$(function () {
+    console.log("JQ and JS")
+
+    $("#submitButton").on("click", addEmployee)
+});
+//readyNow function, on page load whatever is in here happens
+
+function addEmployee() {
+    const table = $("table");
+}
