9ff81fe6266213773c790cd18f1b60f7b9d3b877
User <user@Users-MacBook-Pro.local>
2023-05-05 18:01:25 -0500

    changded around and added more html. While also added and creating more functions for the java.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,24 @@
 $(function () {
     console.log("JQ and JS")
-
+let monthlyCost = 0;
     $("#submitButton").on("click", addEmployee)
 });
 //readyNow function, on page load whatever is in here happens
 
 function addEmployee() {
-    const table = $("table");
+    const tableBody = $("tbody");
+    const firstIn = $("firstIn");
+    const LastIn = $("lastIn");
+    const idIn = $("idIn");
+    const titleIn = $("titleIn");
+    const annualIn = $("annualIn");
+
+    const employeeInfo = `<tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+            <td><button class="deleteButton">Delete</button></td>
+      </tr>`
 }
