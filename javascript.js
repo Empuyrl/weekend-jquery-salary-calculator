1135c474feffa8178d4e1ba822c2477f81b240ba
User <user@Users-MacBook-Pro.local>
2023-05-08 11:48:42 -0500

    changed ids to match, must have changed them without noticing

--- javascript.js
+++ javascript.js
@@ -16,17 +16,13 @@ $(document).ready(function () {
       //parseFloat is used for salary in order to have the value returned as a number instead of a string
       //value is a string because of the form format
       //reminder of basic jquery form $('');
-      const firstName = $('#firstName').val();
-      const lastName = $('#lastName').val();
-      const idNumber = $('#idNumber').val();
-      const Title = $('#Title').val();
-      const annualSalary = parseFloat($('#annualSalary').val());
+      const firstName = $('#firstInput').val();
+      const lastName = $('#lastInput').val();
+      const idNumber = $('#idInput').val();
+      const title = $('#titleInput').val();
+      const annualSalary = parseFloat($('#annualInput').val());
   
-      console.log('Employee details:', firstName, lastName, idNumber, Title, annualSalary);
-    //   if (firstInput.val() === '' || lastInput.val() === '' || idInput.val() === '' || titleInput.val() === '' || annualInput.val() === '') {
-    //         }
-    //     console.log(`The data is coming in: First, ${firstInput}, Last, ${lastInput}, Id, ${idInput}, Title, ${titleInput}, Annual, ${annualInput} `);
-    //not sure why this statement/log doesn;t work, think I'm close. Check more if time allows.  
+      console.log('Employee details:', firstName, lastName, idNumber, title, annualSalary);
     
     // Calculate the monthly salary and add it to the total
     //the 12 is in order to get the monthly, since annual would be yearly
@@ -43,6 +39,7 @@ $(document).ready(function () {
   
       // If the total monthly cost exceeds $20,000, add a red background color
       //also calling to a css class in order to apply a change in color
+      //adding a class using jquery
       if (totalMonthlyCost > 20000) {
         $('#totalMonthlyCost').addClass('red-background');
       }
@@ -51,12 +48,13 @@ $(document).ready(function () {
       //all of the information to be added to the form when submit is clicked
       //also creates a delete button to be used in the form
       //grabbing all of the rows of the table body,not the table itself
+      //delete button can be called action in html as well
       $('#employeeTable').append(
         `<tr>
           <td>${firstName}</td>
           <td>${lastName}</td>
           <td>${idNumber}</td>
-          <td>${Title}</td>
+          <td>${title}</td>
           <td>${annualSalary.toFixed(2)}</td>
           <td><button class="deleteBtn">Delete</button></td>
         </tr>`
@@ -67,25 +65,32 @@ $(document).ready(function () {
       // Clear the input fields
       //values are returned in strings, thus ''
       //id because the .val was used earlier in tge code as well
-      $('#firstName').val('');
-      $('#lastName').val('');
-      $('#idNumber').val('');
-      $('#Title').val('');
-      $('#annualSalary').val('');
+      $('#firstInput').val('');
+      $('#lastInput').val('');
+      $('#idInput').val('');
+      $('#titleInput').val('');
+      $('#annualInput').val('');
   
       console.log('Input fields cleared');
     });
   
     // When a delete button is clicked
     //targeting the body of the table again
+    //don't forget commas when using this method
     $('#employeeTable').on('click', '.deleteBtn', function () {
       console.log('Delete button clicked');
   
       // Remove the corresponding table row
       //closet deletes only the one row when asked
+      //this refers to the element within the event
       $(this).closest('tr').remove();
   
       console.log('Employee removed from the table');
     });
   });
 
+//   if (firstInput.val() === '' || lastInput.val() === '' || idInput.val() === '' || titleInput.val() === '' || annualInput.val() === '') {
+    //         }
+    //     console.log(`The data is coming in: First, ${firstInput}, Last, ${lastInput}, Id, ${idInput}, Title, ${titleInput}, Annual, ${annualInput} `);
+    //not sure why this statement/log doesn;t work, think I'm close. Check more if time allows.  
+    
